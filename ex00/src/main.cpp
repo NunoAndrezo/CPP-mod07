b9bab692545c21ce1bbf92616278bed6ac63e735
nuno <nunoandrezo97@gmail.com>
2026-06-05 18:06:43 +0100

    ex00: changed main

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -20,13 +20,26 @@ std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); 
 
 int main(void)
 {
-	Awesome a(2), b(4);
+	Awesome opa(2), opb(4);
 
-	swap(a, b);
-	std::cout << a << " <- a and b -> " << b << std::endl;
-	std::cout << max(a, b) << std::endl;
-	std::cout << min(a, b) << std::endl;
+	swap(opa, opb);
+	std::cout << opa << " <- a and b -> " << opb << std::endl;
+	std::cout << max(opa, opb) << std::endl;
+	std::cout << min(opa, opb) << std::endl;
 
 	//try min(10, 20) or min(Awesome(5), Awesome(10))
+
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min( a, b ) << std::endl;
+	std::cout << "max(a, b) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min( c, d ) << std::endl;
+	std::cout << "max(c, d) = " << ::max( c, d ) << std::endl;
 	return (0);
 }
